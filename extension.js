b3c3d1df1fcccfa131f85b76b28080368208994f
lhenry2k <e.villa@an4a.com>
2023-10-19 23:59:00 +0200

    Update extension.js
    gpt4

--- extension.js
+++ extension.js
@@ -1,76 +1,19 @@
-
-var vscode = require('vscode');
-var $$path = require('path');
-var $$fs = require('fs');
-
-function activate(context) {
-    // console.log('Congratulations, your extension "savebackup" is now active!');
-
-    var disposable = vscode.commands.registerTextEditorCommand('extension.saveBackup.backupFile', function (textEditor, edit) {
-        backupFile(textEditor.document);
-    });
-    vscode.workspace.onDidSaveTextDocument(function (document) {
-        backupFile(document);
-    });
-
-    context.subscriptions.push(disposable);
-}
-
-function backupFile(document) {
-    var sFileText = document.getText();
-    var sFilePath = document.uri.path;
-    if (sFilePath[0] === '/') {
-        sFilePath = sFilePath.slice(1); 
-    }
-    var oConf = vscode.workspace.getConfiguration('saveBackup.conf');
-
-    if (oConf.enable) {
-        var backupDir = getParsePath(oConf.backupDir);
-        var sBakPath = buildeBakPath(sFilePath, backupDir);
-        if ($$fs.existsSync(backupDir)) {
-            try {
-                var sFileName = $$path.basename(sFilePath);
-                if (new RegExp(oConf.fileNameMatch).test(sFileName)) {
-                    var sFileDir = $$path.dirname(sBakPath);
-                    if (!$$fs.existsSync(sFileDir)) {
-                        $$fs.mkdirSync(sFileDir);
-                    }
-                    $$fs.writeFileSync(sBakPath, sFileText); 
-                }
-            } catch (error) {
-                vscode.window.showErrorMessage(`extension.saveBackup : ${error.message}`);
-            }
-        }else{
-            vscode.window.showErrorMessage(`extension.saveBackup : ${backupDir} is not exists. To create the dir, or configure saveBackup.conf.backupDir`);
-        }
-    }
-}
-
 function buildeBakPath(sFilePath, sBackupDir) {
-    var sNewPath = sFilePath.replace(/[\/:]/g, '_');
-    var sR = $$path.join(sBackupDir, sNewPath);
-    var sExName = $$path.extname(sFilePath);
+    // Get the relative path of the file without the filename
+    var sFileRelDir = $$path.dirname(sFilePath);
+
+    // Join the backup directory with the relative path
+    var sRelBackupPath = $$path.join(sBackupDir, sFileRelDir);
+
+    // Append the timestamp to the folder (all backups of the same file on a given day will be in the same folder)
     var oD = new Date(); 
     var sTime = `${oD.getFullYear()}${c2(oD.getMonth()+1)}${c2(oD.getDate())}`;
     sTime += `_${c2(oD.getHours())}${c2(oD.getMinutes())}${c2(oD.getSeconds())}` + '_' + (+oD).toString().slice(-3);
-    sR = $$path.join(sR, sTime + sExName).replace(/\\/g, '/');
+    sRelBackupPath = $$path.join(sRelBackupPath, sTime);
 
-    return sR;
+    // Append the file name to the path
+    var sFileNameWithExt = $$path.basename(sFilePath);
+    sRelBackupPath = $$path.join(sRelBackupPath, sFileNameWithExt).replace(/\\/g, '/');
+
+    return sRelBackupPath;
 }
-function getParsePath(sPath) {
-    var sVscodeDir = $$path.join(__dirname, '../..'); 
-    sPath = sPath.replace('${.vscode}', sVscodeDir);
-    return sPath.replace(/\\/g, '/');
-}
-function c2(n) {
-    return (n/100).toFixed(2).slice(-2);
-}
-exports.activate = activate;
-
-function deactivate() {
-}
-exports.deactivate = deactivate;
-
-
-
-
